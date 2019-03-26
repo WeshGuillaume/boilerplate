b882ddfedd6d204bae372dfeadbc4d48ade0e332
Guillaume <guillaume.badi@gmail.com>
2019-03-26 21:57:29 +0100

    app

---
+++ src/app/src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components/native'
+
+const Text = styled.Text`
+  color: red;
+`
+
+export default function App() {
+  return <Text>Hello</Text>
+}
